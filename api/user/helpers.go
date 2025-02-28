0a626984ebcc0b6f9f50a6fbef0e854d80afd6ce
FudgyDRS <yetimonsters@gmail.com>
2025-02-28 12:25:02 -0800

    test

--- api/user/helpers.go
+++ api/user/helpers.go
@@ -2,6 +2,7 @@ package userHandler
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"os"
@@ -62,14 +63,51 @@ func ConvertStructToQuery(params interface{}) (string, error) {
 	return query.Encode(), nil
 }
 
+// func sendRequest(api, query, body string) {
+// 	url := fmt.Sprintf("%v?query=%v&%v", api, query, body)
+// 	logrus.Info("Request forwarded: ", url)
+// 	req, err := http.Get(url)
+// 	if err != nil {
+// 		logrus.Error("Request creation error: ", err)
+// 		return
+// 	}
+
+// 	defer req.Body.Close()
+// }
+
 func sendRequest(api, query, body string) {
 	url := fmt.Sprintf("%v?query=%v&%v", api, query, body)
 	logrus.Info("Request forwarded: ", url)
-	req, err := http.Get(url)
+
+	// Create a new request
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		logrus.Error("Request creation error: ", err)
 		return
 	}
 
-	defer req.Body.Close()
+	// Set a whitelisted origin
+	// This needs to be one of the origins that the receiving API accepts
+	whitelistedOrigin := "https://blp-api-vercel.vercel.app"
+	req.Header.Set("Origin", whitelistedOrigin)
+	logrus.Info("Setting Origin header to: ", whitelistedOrigin)
+
+	// Execute the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		logrus.Error("Request error: ", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Read response
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		logrus.Error("Failed to read response body: ", err)
+		return
+	}
+
+	logrus.Infof("Response Status: %d", resp.StatusCode)
+	logrus.Infof("Response Body: %s", string(respBody))
 }
